60eddac2cb1e1c732f3fa39d4ea16b7c9bf1b417
Alexander <1alexvash@gmail.com>
2020-12-22 19:04:42 +0200

    Rendering list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  return (
+    <div className="App">
+      <h1>Idioms</h1>
+      <h2>Choose a letter</h2>
+      {alphabet.map((letter) => letter)}
+    </div>
+  );
 };
 
 export default App;
