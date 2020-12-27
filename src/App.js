ffb21045a724423dbad88e9d956c10838fa5982a
Alexander <1alexvash@gmail.com>
2020-12-27 20:05:09 +0200

    Renedring items if the letter Z is correct

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./scss/main.css";
 
 import idioms from "./data/idioms.json";
 
 const App = () => {
+  const [letter, setLetter] = useState("");
+
   const alphabet = [
     "A",
     "B",
@@ -33,18 +35,33 @@ const App = () => {
     "Z",
   ];
 
-  console.log(idioms);
-
   return (
     <div className="App">
       <h1>Idioms</h1>
       <h2>Choose a letter</h2>
       <div className="letters">
         {alphabet.map((letter) => (
-          <div className="letter" key={letter}>
+          <div
+            className="letter"
+            key={letter}
+            onClick={() => setLetter(letter)}
+          >
             {letter}
           </div>
         ))}
+      </div>
+      <h2>
+        Choosen letter is <b>{letter}</b>
+      </h2>
+      <div className="idioms">
+        {letter === "Z"
+          ? idioms.map((idiom) => (
+              <div className="idiom" key={idiom.name}>
+                <div className="name">{idiom.name}</div>
+                <div className="example">{idiom.example}</div>
+              </div>
+            ))
+          : ""}
       </div>
     </div>
   );
