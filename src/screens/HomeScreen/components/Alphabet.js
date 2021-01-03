0a3eba7c371a38d9f76da1583f039467d013a299
Alexander <1alexvash@gmail.com>
2021-01-03 19:11:42 +0200

    Now letter is active if it is clicked on

--- src/screens/HomeScreen/components/Alphabet.js
+++ src/screens/HomeScreen/components/Alphabet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Alphabet = ({ history }) => {
   const alphabet = [
@@ -30,15 +31,17 @@ const Alphabet = ({ history }) => {
     "Z",
   ];
 
-  function handleClick(letter) {
-    history.push(`/idiom-on-letter/${letter}`);
-  }
-
   return (
     <div className="alphabet">
       {alphabet.map((letter) => (
-        <div className="box" key={letter} onClick={() => handleClick(letter)}>
-          <div className="letter">{letter}</div>
+        <div className="box" key={letter}>
+          <NavLink
+            to={`/idiom-on-letter/${letter}`}
+            className="letter"
+            activeClassName="active"
+          >
+            {letter}
+          </NavLink>
         </div>
       ))}
     </div>
